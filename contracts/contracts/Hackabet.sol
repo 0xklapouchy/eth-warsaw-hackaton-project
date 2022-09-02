6286160130bc8f412b2350bbc875f9a71ac505d2
0xklapouchy <0xklapouchy@gmail.com>
2022-09-02 22:00:37 +0200

    Add deposit and withdraw functions

--- contracts/contracts/Hackabet.sol
+++ contracts/contracts/Hackabet.sol
@@ -5,7 +5,9 @@ pragma solidity 0.8.15;
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/cryptography/draft-EIP712.sol";
 import "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
+import "./libraries/Constants.sol";
 /// @title Hackabet contract
 /// @notice
 contract Hackabet is Ownable, EIP712 {
@@ -18,4 +20,21 @@ contract Hackabet is Ownable, EIP712 {
 
     // solhint-disable-next-line no-empty-blocks
     constructor() EIP712("Hackabet", "1") {}
+
+    function deposit(uint256 amount) external {
+        User storage user = users[msg.sender];
+        user.availableUSD += amount;
+
+        IERC20(Constants.USDC).transferFrom(msg.sender, address(this), amount);
+    }
+
+    function withdraw(uint256 amount) external {
+        User storage user = users[msg.sender];
+
+        require(user.availableUSD >= amount, "Nope");
+
+        user.availableUSD -= amount;
+
+        IERC20(Constants.USDC).transfer(msg.sender, amount);
+    }
 }
