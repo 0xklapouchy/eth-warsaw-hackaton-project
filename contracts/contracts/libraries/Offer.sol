5dfd8cb762bba2371a1825d74d3ae86aa39254c8
0xklapouchy <0xklapouchy@gmail.com>
2022-09-02 23:21:42 +0200

    Add Offer lib

---
+++ contracts/contracts/libraries/Offer.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: GLP-3.0
+
+pragma solidity 0.8.15;
+
+library Offer {
+    using Offer for Data;
+
+    struct Data {
+        uint256 volume;
+        uint256 nonce;
+        uint256 deadline;
+        bytes details;
+    }
+
+    function hashVal(bytes memory details, address maker) internal pure returns (bytes32) {
+        return keccak256(abi.encodePacked(maker, details));
+    }
+
+    function fullTypeHash() internal pure returns (bytes32) {
+        return keccak256("Offer(uint256 volume,uint256 nonce,uint256 deadline)");
+    }
+}
