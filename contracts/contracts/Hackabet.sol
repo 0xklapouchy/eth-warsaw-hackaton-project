92b2e582e77dfa09a0ab6ef332d3ffdd09dd2d7a
0xklapouchy <0xklapouchy@gmail.com>
2022-09-02 21:59:36 +0200

    Add Hackabet contract

---
+++ contracts/contracts/Hackabet.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: GLP-3.0
+
+pragma solidity 0.8.15;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/cryptography/draft-EIP712.sol";
+import "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
+
+/// @title Hackabet contract
+/// @notice
+contract Hackabet is Ownable, EIP712 {
+    // solhint-disable-next-line no-empty-blocks
+    constructor() EIP712("Hackabet", "1") {}
+}
